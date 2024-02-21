e8b6f1de1b5ed87f8b86435c6eba7906b2f4459a
Lord-Sinq <82123439+Lord-Sinq@users.noreply.github.com>
2024-02-20 21:09:54 -0500

    This will be the login branch based of the template main

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <h2>Login</h2>
+      <form @submit.prevent="login">
+        <label for="username">Username:</label>
+        <input v-model="username" type="text" id="username" required />
+  
+        <label for="password">Password:</label>
+        <input v-model="password" type="password" id="password" required />
+  
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
+    },
+    methods: {
+      login() {
+        // Implement login 
+        if (this.username === 'user' && this.password === 'password') {
+          // Redirect to home page
+          this.$router.push('/home');
+        } else {
+          alert('Invalid credentials');
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add styles here */
+  </style>
+  
