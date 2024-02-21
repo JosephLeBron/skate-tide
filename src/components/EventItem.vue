bb1051eca8c9eb19a1d44038ee4609ef9e841cf3
Ethan <1980554+Athen-H@users.noreply.github.com>
2024-02-21 17:01:41 -0500

    add barebones Event view, add /events to router + link on home

---
+++ src/components/EventItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="name">Event Title</h1> <!-- add dynamic class binding here -->
+</template>
+
+<style>
+.name {
+  color:cadetblue;
+}
+.location {
+  color:blueviolet;
+}
+.description {
+  color:black;
+}
+</style>
