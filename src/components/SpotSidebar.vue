a9690bd4659e8778ed55e5ed795b766e98a5be9d
Athen <1980554+Athen-H@users.noreply.github.com>
2024-03-25 18:25:29 -0400

    sidebar wip

--- src/components/SpotSidebar.vue
+++ src/components/SpotSidebar.vue
@@ -1,10 +1,30 @@
 <script setup>
 // import { ref } from 'vue'
-defineProps(['name', 'pos'])
+defineProps(['spot'])
 </script>
 
 
 <template>
-    <h1>Spot: {{ name }}</h1>
-    <h2>Pos: {{ pos['lat'] }}, {{ pos['lng'] }}</h2>
+    <div class="sidebar-img-container">
+        <div class="box">
+            test
+        </div>
+        <h1>Spot: {{ spot['name'] }}</h1>
+        <h2>Pos: {{ spot['pos']['lat'] }}, {{ spot['pos']['lng'] }}</h2>
+    </div>
 </template>
+
+<style>
+.sidebar-img-container {
+    width: 100%;
+    height: 15%;
+    border: 1px;
+}
+.sidebar-container {
+    display: grid;
+    width: 100%;
+    height: 100%;
+    color: black;
+    background-color: plum;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,30 +3,26 @@ import { ref } from 'vue'
 import HomeMap from '../components/HomeMap.vue';
 import SpotSideBar from '../components/SpotSidebar.vue'
 
-const spotSelected = ref(false)
-const spotName = ref('')
-const spotPos = ref(null)
+const showSidebar = ref(false)
+const spot = ref(null)
 
-function onMarkerClick(spot) {
-  spotSelected.value = true
-  spotName.value = spot['name']
-  spotPos.value = spot['pos']
+function onMarkerClick(selected) {
+  showSidebar.value = true
+  spot.value = selected
 }
 </script>
 
 <template>
-  <div v-if="spotSelected" class="container">
+  <div v-if="showSidebar" class="homeview-container">
     <div class="sidebar" style="width: 20%"> 
-      <h1>Spot: {{ spotName }}</h1>
-      <h2>Pos: {{ spotPos['lat'] }}, {{ spotPos['lng'] }}</h2>
-      <!-- <SpotSideBar name={{ spotName.value }}></SpotSideBar> -->
+      <SpotSideBar :spot="spot" />
     </div>
     <div class="map" style="width: 80%"> 
       <HomeMap @marker-click="onMarkerClick"/>
     </div>
   </div>
 
-  <div v-else class="container">
+  <div v-else class="homeview-container">
     <div class="map" style="width: 100%"> 
       <HomeMap @marker-click="onMarkerClick"/>
     </div>
@@ -42,30 +38,23 @@ function onMarkerClick(spot) {
   width: 80%;
   height: 100%;
 }
-.container {
+.homeview-container {
   display: flex;
-  min-width: 100vw;
   position: fixed;
   left: 0;  /* Change back to 0px when finished testing */
+  right: 0;
   bottom: 0;
   z-index: 1;
 }
-.test {
-  position: fixed;
-  left: 10px;
-  color:black;
-}
 
 @media (min-width: 1024px) {
-  .container {
+  .homeview-container {
     top: 112px;
-    max-height: calc(100vh - 112px);
   }
 }
 @media (max-width: 1024px) {
-  .container {
+  .homeview-container {
     top: 44px;
-    max-height: calc(100vh - 44px);
   }
 }
 </style>
