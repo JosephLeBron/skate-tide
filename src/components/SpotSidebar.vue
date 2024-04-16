eea8eb4b47dee45f5a7f15c3a866b438eea37f36
andrew teague <aft9395@uncw.com>
2024-04-16 17:56:29 -0400

    delete pin button on the sidebar

--- src/components/SpotSidebar.vue
+++ src/components/SpotSidebar.vue
@@ -1,5 +1,47 @@
 <script setup>
+import axios from 'axios'
 defineProps(['spot'])
+
+
+
+async function deletePin(name, lat, lon, rating, picture, difficulty) {
+    // setError.value = null;
+    try {
+        // Implement deletion logic here
+        const response = await axios.post('http://localhost:8000/pin/api/delete-pin', {
+        name, lat, lon, rating, picture, difficulty
+    });
+    return response.data;
+    } catch (error) {
+        // setError.value = error;
+        throw error;
+    }
+}
+
+function handleButtonClick(spot) {
+    const confirmDelete = confirm(`Are you sure you want to delete the pin " ${spot.name} "?`);
+    if (confirmDelete) {
+        deletePin(spot.name, spot.pos.lat, spot.pos.lng, spot.rating, spot.picture, spot.difficulty )
+            .then(() => {
+                // Handle deletion success
+                try {
+                    console.log(`Pin " ${spot.name} "deleted Successfully`)
+                    // Remove the deleted pin from the spots array or any other necessary action
+                    // spot.value.push ({
+                    //     name: null,
+                    //     pos: { lat: null, lng: null },
+                    //     rating: null,
+                    //     img: null,
+                    //     difficulty: null
+                    // });
+                } catch (error) {
+                    console.error("An error occurred during pin deletion:", error);
+                    // Handle error here, if needed
+                }
+            })
+            .catch(error => console.error("An error occurred during pin deletion:", error));
+    }
+}
 </script>
 
 <template>
@@ -8,6 +50,7 @@ defineProps(['spot'])
         <div class="sidebar-contents">
             <h1>{{ spot['name'] }}</h1>
             <h2>Pos: {{ spot['pos']['lat'] }}, {{ spot['pos']['lng'] }}</h2>
+            <button class="delete-button" @click="handleButtonClick(spot)">Delete Pin</button>
         </div>
         <button class="close-button" @click="$emit('close-button')">
             < <!-- If this is underlined red, ignore it. It's correct. I'll replace it with an icon some day -->
